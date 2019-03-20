f588a9d0226cb4867375f0b42129a20245b0b71c
gituser <yan2010zsj@163.com>
2019-03-20 16:46:13 +0800

    first

---
+++ springboot_01/src/main/java/com/bjsxt/application/Springboot01Application.java
@@ -0,0 +1,18 @@
+package com.bjsxt.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/****
+ * springboot应用的启动类
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+public class Springboot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot01Application.class, args);
+	}
+
+}
---
+++ springboot_01/src/main/java/com/bjsxt/application/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.bjsxt.application.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * Controller必须放入启动类所在子包中
+ * @author Administrator
+ *
+ */
+//@Controller
+@RestController //组合注解，Controller纳入spring容器，完成异步响应
+public class UserController {
+
+	
+	@RequestMapping("/loadUser")
+	//@ResponseBody
+	public String loadUser() {
+		return "这是用户信息";
+	}
+}
